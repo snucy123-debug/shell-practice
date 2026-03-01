d0da649700aba11f12db7d0ed38c9effed1527ef
vardhan <snucy123@gmail.com>
2026-03-01 20:09:02 +0530

    session-15

--- 09-Special-variables.sh
+++ 09-Special-variables.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "all variables passed through $@" #All variables of script
+echo "all variables passed through :$@" #All variables of script
 
+echo "Number of variables passed on script :$#" #number of variables script
+
+echo "what is the scriptr name : $0" #Script name 
+
+echo "present working directory :$PWD"
+
+echo "which user is running the script :$PWD"
+
+echo "what is the home directory : $HOME"
+
+echo "PID of the script : $$"
+
+echo "SO ABOVE GIVEN INFORMATION IS ----SPECIAL-VARIABLES------"
