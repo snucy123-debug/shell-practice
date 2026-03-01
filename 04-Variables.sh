76684661c4af8dcfa58a6f0be4fbabbba987a73c
vardhan <snucy123@gmail.com>
2026-03-01 17:17:01 +0530

    conservation with friends

---
+++ 04-Variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=RAVI #there should be no space
+PERSON2=THARUN
+
+echo "$PERSON2: hi how are u bro"
+echo "$PERSON1: i am fine , had dinner ?"
+echo "$PERSON2: No $PERSON1 have to eat mom is preparing"
+echo "$PERSON1: Okay have dinner and focus on your goals $PERSON2"
+echo "$PERSON2: Okay bro , i will try!"
+
+
+
+#So here main thing is if u give any values in linux server (mobxtream) after u write sh 04-Variables.sh Bhargavi kushma 
+#so it will take bhargavi as $1 kushma as $2 it wiill change whole paragraph with our given values /arguments.
