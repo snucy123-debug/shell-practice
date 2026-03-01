05090e7e36efee5bddd1e939edf3616aa0d68775
vardhan <snucy123@gmail.com>
2026-03-01 17:04:21 +0530

    conservation with friends

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=AVINASH
+PERSON2=VARDHAN
+
+echo "$PERSON2: hi how are u bro"
+echo "$PERSON1: i am fine , had dinner vardhan ?"
+echo "$PERSON2 : No $PERSON1 have to eat mom is preparing"
+echo "$PERSON1: okay after having lunch call me  $PERSON2"
