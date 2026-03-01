f0b638c4a752a1fef1891ed09bccf1f93cb2156f
vardhan <snucy123@gmail.com>
2026-03-01 20:54:31 +0530

    session-15

--- 10-Conditions.sh
+++ 10-Conditions.sh
@@ -5,7 +5,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 65 ]
 then 
-echo "$NUMBER is lessthan 65"
+echo "$NUMBER is greaterthan  65"
 else
-echo "$NUMBER is not lessthan 65"
+echo "$NUMBER is not no greaterthan  65"
 fi
