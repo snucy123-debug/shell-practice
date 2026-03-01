bce6921d8a0c1e74e05d7c63f321deba430c6670
vardhan <snucy123@gmail.com>
2026-03-01 16:52:41 +0530

    conservation with friends

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,6 +2,6 @@
 echo "me: hi how are u bro"
 echo "avi: i am fine , had dinner vardhan ?"
 echo "me : No avi have to eat mom is preparing"
-echo :avi: oka ra have and call me bai"
- 
+echo "avi: oka ra have and call me bai"
+
 
