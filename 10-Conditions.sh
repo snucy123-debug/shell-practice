9c855bd84e5f1bdca2b79341795a02fe48548c4d
vardhan <snucy123@gmail.com>
2026-03-01 20:51:42 +0530

    session-15

--- 10-Conditions.sh
+++ 10-Conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Find the  number number less than 10 or not ?
+#Find the number less than 65 or not ?
 
 NUMBER=$1
-if [ $NUMBER -lt 10 ]
+if [ $NUMBER -gt 65 ]
 then 
-echo "$NUMBER is lessthan 10"
+echo "$NUMBER is lessthan 65"
 else
-echo "$NUMBER is not lessthan 10"
+echo "$NUMBER is not lessthan 65"
 fi
