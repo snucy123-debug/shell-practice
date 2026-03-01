80a54a3cb6156929b76f09937eaf407c967804a0
vardhan <snucy123@gmail.com>
2026-03-01 17:09:31 +0530

    conservation with friends

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -7,5 +7,5 @@ echo "$PERSON2: hi how are u bro"
 echo "$PERSON1: i am fine , had dinner ?"
 echo "$PERSON2: No $PERSON1 have to eat mom is preparing"
 echo "$PERSON1: Okay have dinner and focus on your goals $PERSON2"
-echo "$PERSON2: Okay bro , i will try !
-        
+echo "$PERSON2: Okay bro , i will try!"
+
