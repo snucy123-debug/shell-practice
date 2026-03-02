a2b63f19d70e9389444a61bf28cfee5be3e37911
vardhan <snucy123@gmail.com>
2026-03-02 14:39:49 +0530

    Practice conditions

--- 11-Conditions-2.sh
+++ 11-Conditions-2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-USERID=$(id-u)  
+USERID=$(id -u)  
 # Here we use $() to store the command date so we use it.“Run this command and store its OUTPUT”
 if  [ $USERID -ne 0 ]  
 # Here we use [] for in conditions to text 
 then
     echo "SORRY!!! YOUR NOT RUNNING IN ROOT ACCESS SO PLEASE USE SUDO SU"
-    exit 1 # give exit code if it gives othher than 0 upto 126
+    exit 1  #we took exit 1 for if user id is  not equal to 0 
+            #then they are not using root acess so it have to stop there so if we donnot keep exit 1 here 
+            #then it will go to "else" condition also so we use exit code. to stop the script immediately"
 else
     echo "YOUR SUCESSFULLY USING ROOT ACCESS PLEASE CONTINUE"
 
