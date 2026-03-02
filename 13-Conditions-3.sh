7de05ead067cbb2b32340e8bc90d458f911d8f3c
vardhan <snucy123@gmail.com>
2026-03-02 17:12:36 +0530

    Practice conditions

--- 13-Conditions-3.sh
+++ 13-Conditions-3.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 #-----------------------"INSTALL MY SQL"-------------------#
 
-USERID=$(id -u) # 1st we check weatcher user using root access or not ?
 
+#01►ABOVE FOR ROOT ACCESS.
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "FOR MYSQL  installation please use in root access"
-exit 1
+echo "please use root access"
 else
-echo "sucessfully ur with root acess please continue to install it "
+echo "Yes !!! youu were in root acess please continue..."
 fi
 
-dnf list installed mysql #checking weather MYSQL already  installed or not ?
+#02► CHECK WEATHER MY SQL IS ALREADY INSTALLED OR NOT ?.
 
-if [ $? -ne 0 ]
+dnf list installed mysql
+if[ $? -eq 0]
 then
-echo "MY SQL IS NOT INSTALLED GOING TO INSTALL IT NOW"
-      #So if exit code ($?) is not equal to 0 then my sql not installed so check below.
-    dnf install mysql -y
-      if [ $? -eq 0 ]
-      then 
-       echo "my sql installing sucessfully..........."
-      else
-       echo "ERROR!! My sql not installing...........please try again"
-       exit 1
-      fi
-      else 
-     echo  "my sql is already installed"
-     fi
+echo "my sql already installed"
+else
+echo "my  sql not yet iinstalled will install now"
+dnf install mysql
+if [ $? -eq 0 ]
+then 
+echo "my sql installed sucessfully...."
+else
+echo "my sql not installed"
+fi
+fi
