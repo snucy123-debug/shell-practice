3abadc321ac81aef335c977ea0525e61fe3d8004
vardhan <snucy123@gmail.com>
2026-03-02 13:57:54 +0530

    Practice conditions

---
+++ 11.Conditions-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+if ( $USERID -nq 0 )
+
+then
+    echo "SORRY!!! YOUR NOT RUNNING IN ROOT ACCESS SO PLEASE USE SUDO SU"
+else
+    echo "YOUR SUCESSFULLY USING ROOT ACCESS PLEASE CONTINUE"
+
+    fi
+    
