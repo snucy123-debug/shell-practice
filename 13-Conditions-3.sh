2058cfcdbf5fc22ac9f27980a6e8aebfae1dc7f5
vardhan <snucy123@gmail.com>
2026-03-02 15:44:43 +0530

    Practice conditions

--- 13-Conditions-3.sh
+++ 13-Conditions-3.sh
@@ -10,17 +10,23 @@ exit 1
 else
 echo "sucessfully ur with root acess please continue to install it "
 fi
+
 dnf list installed mysql #checking weather MYSQL already  installed or not ?
 
 if [ $? -ne 0 ]
 then
 echo "MY SQL IS NOT INSTALLED GOING TO INSTALL IT NOW"
       #So if exit code ($?) is not equal to 0 then my sql not installed so check below.
-      dnf install mysql -y
+    dnf install mysql -y
       if [ $? -eq 0 ]
       then 
-      echo "my sql installing sucessfully..........."
+       echo "my sql installing sucessfully..........."
       else
-      echo "ERROR!! My sql not installing...........please try again"
-      exit 1
+       echo "ERROR!! My sql not installing...........please try again"
+       exit 1
       fi
+     lse
+    echo "MySQL is already installed...Nothing to do"
+fi
+ 
+
