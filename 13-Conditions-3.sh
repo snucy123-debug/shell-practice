3f54a9d2d40da442c6bb4bfb728b36803f7b75e7
vardhan <snucy123@gmail.com>
2026-03-02 17:27:58 +0530

    Practice conditions

--- 13-Conditions-3.sh
+++ 13-Conditions-3.sh
@@ -7,8 +7,10 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "please use root access"
+exit 1
 else
 echo "Yes !!! you were in root acess please continue..."
+
 fi
 
 #02► CHECK WEATHER MY SQL IS ALREADY INSTALLED OR NOT ?.
@@ -17,8 +19,9 @@ dnf list installed mysql
 if[ $? -eq 0]
 then
 echo "my sql already installed"
+exit
 else
-echo "my  sql not yet iinstalled will install now"
+echo "my  sql not yet installed will install now"
 
 #03►IF MYSQL NOT YET INSTALLED WILL INSTALL IT NOW.
 
